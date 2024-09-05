11fbe72c6827b534b52aa75dcaa0f6916fa2d6ea
ALI Nawar <nr.ali1912@gmail.com>
2024-09-05 15:51:23 +0200

    implemented-search-apartments-by-date

---
+++ src/Bookify.Application/Apartments/SearchApartmentsByDate/SearchApartmentsByDateQuery.cs
@@ -0,0 +1,6 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+
+namespace Bookify.Application.Apartments.SearchApartmentsByDate;
+public sealed record SearchApartmentsByDateQuery(
+    DateOnly StartDate,
+    DateOnly EndDate) : IQuery<IReadOnlyList<ApartmentResponse>>;
