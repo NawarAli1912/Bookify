c0993cb0c0d7abc41e6c020927c8fb9b579d7bef
ALI Nawar <nr.ali1912@gmail.com>
2024-09-05 10:34:12 +0200

    implemented-reserve-booking

---
+++ src/Bookify.Domain/Apartments/ApartmentErrors.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstraction;
+
+namespace Bookify.Domain.Apartments;
+public static class ApartmentErrors
+{
+    public static Error NotFound = new(
+        "Apartment.NotFound",
+        "The apartment with the specified identifier was not found");
+}
